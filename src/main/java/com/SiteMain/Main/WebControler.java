dc8d3d708396e0fb4c1bd012180cb9cd200a886d
João Marcelo <joaosantiago558@gmail.com>
2023-11-19 22:36:00 -0300

    alterações nos estilos CSS

--- src/main/java/com/SiteMain/Main/WebControler.java
+++ src/main/java/com/SiteMain/Main/WebControler.java
@@ -19,13 +19,11 @@ public class WebControler {
     @RequestMapping("/Menuindex")
     public String Menuindex(Model modelo){
         return "Menuindex";
-                
     }
     
     @RequestMapping("/cadastro")
     public String login(Model modelo){
         return "cadastro";
-                
     }
     
     @RequestMapping("/catalogo")
@@ -37,19 +35,20 @@ public class WebControler {
     public String contato(Model modelo){
         System.out.println("Chamando o metodo SayHello");
         return "contato";
-                
     }
     
     @RequestMapping(value = "teste", method = RequestMethod.POST)
     public String Cadastro(Model modelo, String InputName,String InputNick, String InputEmail, String InputSenha, String InputSenhaConfirm){
-        
+            //Instanciando objetos para realização dos metodos
             Doc doc = new Doc();
             DbMongoDB mongo = new DbMongoDB();
             DbSQL SQL = new DbSQL();
-            
+            //Instanciando objetos da classe usuario 
+            //U2 usuario para validação para verificar a existencia de um usuario
+            //U1 usuario criado durante o cadastro
             User U2 = SQL.sqlUserSelectNickName(InputNick);
             User U1 = new User(0,InputName,InputNick, InputEmail, InputSenha);
-            
+            //Verificando a existencia de um usuario com o mesmo Nickname
             if(U2.getNickName().equals(InputNick)){
                  modelo.addAttribute("mensagem", "Usuario ja existente: "+ U1.getNickName());
                  return "login";
@@ -58,8 +57,6 @@ public class WebControler {
                 SQL.sqlDbUserInsert(U1);
                 modelo.addAttribute("mensagem", "Cadastro Realizado: "+ U1.getNickName());
             }
-            
         return "login";
-        
     }
 }
